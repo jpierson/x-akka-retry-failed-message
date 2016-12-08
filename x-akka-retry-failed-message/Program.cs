2e8947499b20dc50ca4a627a16ea132e1df7671c
Jeff <jeffrey.pierson@gmail.com>
2016-12-07 21:42:03 -0500

    Add project files.

---
+++ x-akka-retry-failed-message/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_akka_retry_failed_message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
