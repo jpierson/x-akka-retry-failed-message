c25d27f66e536cfb654f2b8756d5279321620215
Jeff <jeffrey.pierson@gmail.com>
2016-12-07 23:42:19 -0500

    Add initial sample actor systems

---
+++ x-akka-retry-failed-message/ActorSystem1.cs
@@ -0,0 +1,76 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using x_akka_retry_failed_message;
+
+namespace ActorSystem1
+{
+    public class ParentActor : ReceiveActor
+    {
+        private IActorRef child;
+
+        public ParentActor()
+        {
+            child = Context.ActorOf(Props.Create<ChildActor>());
+
+            Receive<int>(i =>
+            {
+                // manullay forward the message
+                child.Tell(i);
+            });
+        }
+    }
+
+    public class ChildActor : ReceiveActor, IWithUnboundedStash
+    {
+        public IStash Stash { get; set; }
+
+        public ChildActor()
+        {
+            Receive<int>(i =>
+            {
+                Stash.Stash();
+
+                if (Program.RandomGeneator.Next(0, 20) == 1)
+                    throw new InvalidOperationException(string.Format("Sorry you were randomly chosen to fail ({0})", DateTime.Now.Second));
+
+                using (var file = new FileStream(Assembly.GetEntryAssembly().Location, FileMode.Open, FileAccess.Read))
+                {
+                    var buffer = new byte[512];
+                    file.Read(buffer, 0, buffer.Length);
+                }
+
+                Stash.ClearStash();
+                Console.WriteLine(i);
+
+                if (i >= 1000)
+                {
+                    Context.System.Terminate();
+                }
+            });
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0} failed", message);
+
+            Stash.Unstash();
+            Thread.Sleep(1000);
+
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+        }
+    }
+}
--- x-akka-retry-failed-message/Program.cs
+++ x-akka-retry-failed-message/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using ActorSystem1;
+using Akka.Actor;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace x_akka_retry_failed_message
 {
     class Program
     {
+        public static Random RandomGeneator = new Random(101);
+
         static void Main(string[] args)
         {
+            var system = ActorSystem.Create("test");
+
+            var actor = system.ActorOf(Props.Create<ActorSystem2.ParentActor>());
+
+            foreach (var i in Enumerable.Range(1, 1000))
+            {
+                actor.Tell(i);
+            }
+
+
+            system.WhenTerminated.Wait();
         }
     }
+
+
 }
